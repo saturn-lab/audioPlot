64461400bbba5dbe78434a91e710dfa2c19ff0c1
haotian <ritou11@gmail.com>
2020-04-20 12:18:31 +0800

    fix bugs & improve codes

--- wavfile2waveform.py
+++ wavfile2waveform.py
@@ -1,47 +1,46 @@
-#!/opt/anaconda3/bin/python
 # -*- coding:utf-8 -*-
 
 import os
+import os.path as osp
 import glob
 import sys
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 from wavReader import readWav
 
 def ConvertFile2Waveform(audio, dir_out):
-  fname = os.path.basename(audio)
-  
-  rate, data =readWav(audio)
-	
-  fname = fname.split('.')[0]
-  
-  fname = os.path.join(dir_out, fname)
-  
-  print(fname)
-  
-  plt.plot(data)
-  
-  plt.savefig(fname + '.png')
+    fname = osp.basename(audio)
 
-  plt.close()
-  
-  return True
- 
-def ConvertAudioToWaveform(dir_in, dir_out=os.path.join('.', 'image', 'waveform')):
-  if not os.path.exists(dir_in):
-    raise(ValueError(dir_in + ' does not exist'))
-	
-  if not os.path.exists(dir_out):
-    raise(ValueError(dir_out + ' does not exist'))
-	
-  audios = glob.glob(os.path.join(dir_in, '*.*'))
-  
-  print(audios)
-  
-  for audio in audios:
-    ConvertFile2Waveform(audio, dir_out)
-  return
-  
+    rate, data = readWav(audio)
+
+    fname = fname.split('.')[0]
+
+    fname = osp.join(dir_out, fname)
+
+    print(fname)
+
+    plt.plot(data)
+    plt.savefig(fname + '.png')
+
+    return True
+
+
+def ConvertAudioToWaveform(dir_in, dir_out=osp.join('.', 'image', 'waveform')):
+    if not osp.exists(dir_in):
+        raise(ValueError(dir_in + ' does not exist'))
+
+    if not osp.exists(dir_out):
+        raise(ValueError(dir_out + ' does not exist'))
+
+    audios = glob.glob(osp.join(dir_in, '*.*'))
+
+    print(audios)
+
+    for audio in audios:
+        ConvertFile2Waveform(audio, dir_out)
+    return
+
+
 if __name__ == '__main__':
-  ConvertAudioToWaveform(sys.argv[1])
+    ConvertAudioToWaveform(sys.argv[1])
